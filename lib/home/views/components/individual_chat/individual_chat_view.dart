99aad46892cf62d34854707193eccf9ef8c17a33
Fairouz Khalaf <159648169+fairouz-khalaf@users.noreply.github.com>
2025-04-23 17:03:33 +0200

    trying to show emoji

--- lib/home/views/components/individual_chat/individual_chat_view.dart
+++ lib/home/views/components/individual_chat/individual_chat_view.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/home/data/models/chat_model.dart';
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:marquee/marquee.dart';
@@ -12,108 +13,252 @@ class IndividualChatView extends StatefulWidget {
 }
 
 class _IndividualChatViewState extends State<IndividualChatView> {
+  bool showEmoji = false;
+  final TextEditingController textEditingController = TextEditingController();
+  final FocusNode focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    focusNode.addListener(() {
+      if (focusNode.hasFocus) {
+        setState(() {
+          showEmoji = false;
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          leadingWidth: 90.w,
-          titleSpacing: 0,
-          backgroundColor: Color(0xFF075E54),
-          leading: InkWell(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.arrow_back_ios, color: Colors.white),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: SafeArea(
+        child: WillPopScope(
+          onWillPop: () {
+            if (showEmoji) {
+              setState(() {
+                showEmoji = false;
+              });
+              return Future.value(false);
+            }
+            return Future.value(true);
+          },
+          child: Scaffold(
+            backgroundColor: Colors.blueGrey.withAlpha(230),
+            appBar: AppBar(
+              leadingWidth: 90.w,
+              titleSpacing: 0,
+              backgroundColor: const Color(0xFF075E54),
+              leading: InkWell(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: const Icon(
+                        Icons.arrow_back_ios,
+                        color: Colors.white,
+                      ),
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                    const CircleAvatar(child: Icon(Icons.person)),
+                  ],
                 ),
-                CircleAvatar(
-                  child:
-                      widget.chatModel.isGroup
-                          ? Icon(Icons.groups)
-                          : Icon(Icons.person),
+              ),
+              title: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.chatModel.name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.h,
+                      width: 200.w,
+                      child: Marquee(
+                        text: 'Last seen today at 2:30 PM',
+                        style: TextStyle(fontSize: 14, color: Colors.white),
+                        scrollAxis: Axis.horizontal,
+                        blankSpace: 20.0,
+                        velocity: 30.0,
+                        pauseAfterRound: Duration(seconds: 1),
+                        startPadding: 10.0,
+                        accelerationDuration: Duration(seconds: 1),
+                        accelerationCurve: Curves.linear,
+                        decelerationDuration: Duration(milliseconds: 500),
+                        decelerationCurve: Curves.easeOut,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.videocam, color: Colors.white),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Icon(Icons.call, color: Colors.white),
+                  onPressed: () {},
+                ),
+                PopupMenuButton(
+                  icon: const Icon(Icons.more_vert, color: Colors.white),
+                  itemBuilder:
+                      (context) => [
+                        const PopupMenuItem(
+                          value: 1,
+                          child: Text('View Contact'),
+                        ),
+                        const PopupMenuItem(
+                          value: 2,
+                          child: Text('Media, Links, and Docs'),
+                        ),
+                        const PopupMenuItem(value: 3, child: Text('Search')),
+                        const PopupMenuItem(
+                          value: 4,
+                          child: Text('Mute Notifications'),
+                        ),
+                        const PopupMenuItem(value: 5, child: Text('Wallpaper')),
+                      ],
+                  onSelected: (value) {
+                    print(value);
+                  },
                 ),
               ],
             ),
-          ),
-          title: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: InkWell(
-              onTap: () {},
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    widget.chatModel.name,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                      color: Colors.white,
+            body: Column(
+              children: [
+                Expanded(child: ListView()),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width - 55.w,
+                      child: Card(
+                        margin: EdgeInsets.only(
+                          left: 5.w,
+                          right: 2.w,
+                          bottom: 8.h,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25.r),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10.w),
+                          child: TextFormField(
+                            controller: textEditingController,
+                            focusNode: focusNode,
+                            textAlignVertical: TextAlignVertical.center,
+                            maxLines: 6,
+                            minLines: 1,
+                            keyboardType: TextInputType.multiline,
+                            onTap: () {
+                              setState(() {
+                                showEmoji = false;
+                              });
+                            },
+                            decoration: InputDecoration(
+                              hintText: 'Type a message ...',
+                              border: InputBorder.none,
+                              prefixIcon: IconButton(
+                                icon: const Icon(
+                                  Icons.emoji_emotions_outlined,
+                                  color: Color(0xFF075E54),
+                                ),
+                                onPressed: () {
+                                  FocusScope.of(context).unfocus();
+                                  Future.delayed(
+                                    const Duration(milliseconds: 100),
+                                    () {
+                                      setState(() {
+                                        showEmoji = !showEmoji;
+                                      });
+                                    },
+                                  );
+                                },
+                              ),
+                              suffixIcon: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  IconButton(
+                                    icon: const Icon(
+                                      Icons.attach_file,
+                                      color: Color(0xFF075E54),
+                                    ),
+                                    onPressed: () {},
+                                  ),
+                                  IconButton(
+                                    icon: const Icon(
+                                      Icons.camera_alt,
+                                      color: Color(0xFF075E54),
+                                    ),
+                                    onPressed: () {},
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        bottom: 12.h,
+                        left: 5.w,
+                        right: 5.w,
+                      ),
+                      child: CircleAvatar(
+                        backgroundColor: const Color(0xFF075E54),
+                        radius: 22.r,
+                        child: IconButton(
+                          icon: const Icon(Icons.mic, color: Colors.white),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                if (showEmoji)
                   SizedBox(
-                    height: 20, // تأكد إنه كافي لعرض النص
-                    width: 200, // اضبط العرض حسب المساحة المتاحة
-                    child: Marquee(
-                      text: 'Last seen today at 2:30 PM',
-                      style: const TextStyle(fontSize: 14, color: Colors.white),
-                      scrollAxis: Axis.horizontal,
-                      blankSpace: 20.0,
-                      velocity: 30.0,
-                      pauseAfterRound: Duration(seconds: 1),
-                      startPadding: 10.0,
-                      accelerationDuration: Duration(seconds: 1),
-                      accelerationCurve: Curves.linear,
-                      decelerationDuration: Duration(milliseconds: 500),
-                      decelerationCurve: Curves.easeOut,
+                    height: 250.h,
+                    child: EmojiPicker(
+                      textEditingController: textEditingController,
+                      config: Config(
+                        height: 256,
+                        emojiViewConfig: EmojiViewConfig(
+                          columns: 7,
+                          emojiSizeMax:
+                              32 *
+                              (Theme.of(context).platform == TargetPlatform.iOS
+                                  ? 1.30
+                                  : 1.0),
+                        ),
+                        categoryViewConfig: const CategoryViewConfig(
+                          backgroundColor: Colors.white,
+                          indicatorColor: Color(0xFF075E54),
+                          iconColorSelected: Color(0xFF075E54),
+                        ),
+                        bottomActionBarConfig: const BottomActionBarConfig(
+                          backgroundColor: Colors.white,
+                          buttonColor: Color(0xFF075E54),
+                        ),
+                        searchViewConfig: const SearchViewConfig(
+                          backgroundColor: Colors.white,
+
+                          hintText: 'Search Emoji...',
+                        ),
+                      ),
                     ),
                   ),
-                ],
-              ),
+              ],
             ),
           ),
-
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.videocam, color: Colors.white),
-              onPressed: () {
-                // Add your action here
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.call, color: Colors.white),
-              onPressed: () {
-                // Add your action here
-              },
-            ),
-            PopupMenuButton(
-              itemBuilder: (context) {
-                return [
-                  const PopupMenuItem(value: 1, child: Text('View Contact')),
-                  const PopupMenuItem(
-                    value: 2,
-                    child: Text('Media, Links, and Docs'),
-                  ),
-                  const PopupMenuItem(value: 3, child: Text('Search')),
-                  const PopupMenuItem(
-                    value: 4,
-                    child: Text('Mute Notifications'),
-                  ),
-                  const PopupMenuItem(value: 5, child: Text('Wallpaper')),
-                ];
-              },
-              icon: const Icon(Icons.more_vert, color: Colors.white),
-              onSelected: (value) {
-                print(value);
-              },
-            ),
-          ],
         ),
-        body: Center(child: Text('Chat with ${widget.chatModel.name}')),
       ),
     );
   }
